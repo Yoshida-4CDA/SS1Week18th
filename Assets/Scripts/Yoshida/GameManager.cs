d9c2b547e5264125e8c5863a01390d38a6a57252
Yoshida-4CDA <85817467+Yoshida-4CDA@users.noreply.github.com>
2021-10-03 00:15:01 +0900

    敵の移動(プレイヤーの追跡)(仮)

---
+++ Assets/Scripts/Yoshida/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public bool playerTurn = true;
+    public bool enemyTurn = false;
+
+    Enemy enemy;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        enemy = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Enemy>();
+    }
+
+    void Update()
+    {
+        if (playerTurn || enemyTurn)
+        {
+            return;
+        }
+        StartCoroutine(MoveEnemies());
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemyTurn = true;
+        yield return new WaitForSeconds(0.1f);
+
+        enemy.MoveEnemy();
+        yield return new WaitForSeconds(0.1f);
+
+        enemyTurn = false;
+        playerTurn = true;
+    }
+}
---
+++ Assets/Scripts/Yoshida/Player.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("移動中かどうかを判別する変数")]
+    [SerializeField] bool isMoving;
+
+    int axisX;
+    int axisY;
+
+    BoxCollider2D boxCollider2D;
+    [SerializeField] LayerMask blockingLayer;
+
+    void Start()
+    {
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        axisX = (int)Input.GetAxisRaw("Horizontal");
+        axisY = (int)Input.GetAxisRaw("Vertical");
+
+        if (axisX != 0)
+        {
+            axisY = 0;
+
+            if (axisX < 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else if (axisX > 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+        }
+        else if (axisY != 0)
+        {
+            axisX = 0;
+        }
+
+        if (axisX != 0 || axisY != 0)
+        {
+            // 移動
+            ATMove(axisX, axisY);
+        }
+    }
+
+    public void ATMove(int x, int y)
+    {
+        RaycastHit2D hit;
+
+        // Move関数を呼んでRayを飛ばす => あとあと使うかも?
+        bool canMove = Move(x, y, out hit);
+
+        // Rayにぶつかるものが無ければ移動できる
+        if (hit.transform == null)
+        {
+            // プレイヤーのターン終了
+            GameManager.instance.playerTurn = false;
+            return;
+        }
+        // プレイヤーのターン終了
+        GameManager.instance.playerTurn = false;
+    }
+
+    public bool Move(int x, int y, out RaycastHit2D hit)
+    {
+        Vector2 startPos = transform.position;          // プレイヤーの現在位置
+        Vector2 endPos = startPos + new Vector2(x, y);  // 移動したい位置
+
+        // 自身のBoxCollider2DにRayが反応してしまわないように一旦falseにする
+        boxCollider2D.enabled = false;
+
+        // Rayを飛ばす(開始位置, 終了位置, 判定するLayer)
+        hit = Physics2D.Linecast(startPos, endPos, blockingLayer);
+
+        boxCollider2D.enabled = true;
+
+        // 何もぶつかるものが無ければ移動できる
+        if (!isMoving && hit.transform == null)
+        {
+            StartCoroutine(Movement(endPos));
+            return true;
+        }
+        return false;
+    }
+
+    IEnumerator Movement(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        float distance = (transform.position - targetPos).sqrMagnitude; // 現在地と目的地との距離
+        while (distance > float.Epsilon)
+        {
+            Vector3 currentPos = this.gameObject.transform.position;
+            transform.position = Vector3.MoveTowards(currentPos, targetPos, 6f * Time.deltaTime);
+            distance = (transform.position - targetPos).sqrMagnitude;
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+}
